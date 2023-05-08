efae2bc2bb80aa68716ae54893b9197350bb85c7
lokesh <lokeshkhati005@gmail.com>
2023-05-08 22:38:53 +0530

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Navbar } from "./components"
+
 const App = () => {
 
   return (
     <div className="  text-3xl font-bold underline">
+      <Navbar />
       App
     </div>
   )
 }
 
 export default App
+
+
+
---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+import Home from './Home'
+import Cart from './Cart'
+import About from './About'
+import Contact from './Contact'
+import Register from './Register'
+import Login from './Login'
+import NotFound from './NotFound'
+
+
+export { Home, Cart, About, Contact, Register, Login, NotFound }
